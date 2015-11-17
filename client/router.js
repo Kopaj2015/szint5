ebf37b3bb0ced0dc012e91b69747a8024f676bc3
Pityuasd <debreceni.ad@gmail.com>
2015-11-17 23:32:20 +0100

    new routes

--- client/router.js
+++ client/router.js
@@ -18,14 +18,46 @@ Router.route('/', {
   } 
 });
 
+Router.route('/leaderboard', {
+  name:     'leaderboard',
+  template: 'leaderboard',
+  subscriptions: function() {
+    return [
+      Meteor.subscribe('events'),
+      Meteor.subscribe('categories'),
+      Meteor.subscribe('kopajUsers')
+    ];
+  } 
+});
+
+
 Router.route('/admin', {
   name:     'admin',
   template: 'admin',
   onBeforeAction: function (pause) {
     	if (!Meteor.user()) {
             // render the login template but keep the url in the browser the same
-            this.render('visitor');
+            Router.go('/');
         }
+    },
+  subscriptions: function() {
+    return [
+      Meteor.subscribe('events'),
+      Meteor.subscribe('categories'),
+      Meteor.subscribe('kopajUsers')
+    ];
+  } 
+});
+
+Router.route('/user', {
+  name:     'user',
+  template: 'user',
+  onBeforeAction: function (pause) {
+    	if (!Meteor.user()) {
+            // render the login template but keep the url in the browser the same
+            Router.go('/');
+        }
+    },
   subscriptions: function() {
     return [
       Meteor.subscribe('events'),
