da7d3b07a67e3a8964e92a365b3971001028ebf3
Pityuasd <debreceni.ad@gmail.com>
2015-11-17 23:50:20 +0100

    helper for latest games

--- client/pages/visitor.js
+++ client/pages/visitor.js
@@ -1,8 +1,12 @@
 Template.visitor.helpers({
 
-getCategories: function() {    return categories.find({});
-  }
-  
+	getCategories: function() {
+    	return Categories.find({});
+  	},
+
+  	getLatestEvents: function(){
+  		return Events.find({}, { sort: { date: -1, limit: 5}});
+  	}
 });
 
 
